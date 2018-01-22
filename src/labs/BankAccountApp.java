8cdd91f0b672cb0e2e79428cbc19293a44eaad9a
Cameron <cameronsills1001@gmail.com>
2018-01-22 11:25:39 -0500

    A second commit with some minor code changes

--- src/labs/BankAccountApp.java
+++ src/labs/BankAccountApp.java
@@ -3,10 +3,11 @@ package labs;
 public class BankAccountApp {
 
 	public static void main(String[] args) {
-		BankAccount acc1 = new BankAccount("123456789");
-		BankAccount acc2 = new BankAccount("134356589");
-		BankAccount acc3 = new BankAccount("534566789");
-
+		BankAccount acc1 = new BankAccount("123434789","Cameron Sills",4500);
+		acc1.deposit(1400.45);
+		acc1.deposit(24.95);
+		System.out.println("Balance: " + acc1.getBalance());
+		
 	}
 
 }
@@ -15,23 +16,44 @@ class BankAccount {
 	//properties of bank account
 	private static int id = 1000;
 	private String accountNumber; //id, random 2-digit number + first 2 of SSN
-	private String routingNumber;
+	private static final String routingNumber = "123456789";
 	private String name;
 	private String SSN;
 	private double balance;
 	
+	
 	//constructors
 	
-	public BankAccount(String SSN) {
+	public BankAccount(String SSN, String name, double initDeposit) {
 		System.out.println("New Account Created.");
 		id++;
 		this.SSN = SSN;
+		this.name = name;
+		balance = initDeposit;
 		setAccountNumber();
+		System.out.println(toString());
 	}
 	
 	private void setAccountNumber() {
 		int random = (int)(Math.random() * 100);
 		accountNumber = id + "" + random + SSN.substring(0,2);
 		System.out.println(accountNumber);
+	}
+	
+	public void deposit(double deposit) {
+		balance += deposit;
+	}
+	
+	public void withdraw(double amount) {
+		balance -= amount;
+	}
+	
+	public double getBalance(){
+		return balance;
+	}
+	
+	@Override
+	public String toString(){
+		return "Name: " + name + "\nAccount Nuber: " + accountNumber + "\nRouting Number: " + routingNumber + "\nBalance: " + balance;
 	}
 }
